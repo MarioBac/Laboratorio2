2fe26dd82f20f6535d1176cfb0827ab4d09c860e
Mario Bac <mariobacparedes@gmail.com>
2022-02-18 15:23:29 -0600

    Ejercicio 18 de febrero 2022

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -132,6 +132,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,8 @@ namespace WindowsFormsApp2
                 {
                     uri = "http://" + uri;
                     webBrowser1.Navigate(uri);
-
+                    comboBox1.Items.Add(uri);
+                    Guardar("Historial.txt", uri);
                 }
 
 
@@ -35,11 +37,18 @@ namespace WindowsFormsApp2
                 {
                     uri = "http://www.google.com/search?q=" + uri;
                     webBrowser1.Navigate(uri);
+                    comboBox1.Items.Add(uri);
+                    Guardar("Historial.txt", uri);
                 }
             }
             else {
                 webBrowser1.Navigate(new Uri(comboBox1.SelectedItem.ToString()));
+                string uri = comboBox1.Text.ToString();
+                comboBox1.Items.Add(uri);
+                Guardar("Historial.txt", uri);
             }
+           
+            
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -63,7 +72,17 @@ namespace WindowsFormsApp2
             webBrowser1.GoBack();
         }
 
-        
+        private void Guardar(string fileName, string texto)
+        {
+           
+            FileStream stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+           
+            StreamWriter writer = new StreamWriter(stream);
+  
+            writer.WriteLine(texto);
+            
+            writer.Close();
+        }
 
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
@@ -75,6 +94,29 @@ namespace WindowsFormsApp2
             
                 this.Close();
             
+        }
+
+       private void Leer (string fileName)
+        {
+            
+
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+
+            while (reader.Peek() > -1)
+
+            {
+                comboBox1.Items.Add(reader.ReadLine());
+            }
+
+            reader.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Leer("Historial.txt");
         }
     }
 }
